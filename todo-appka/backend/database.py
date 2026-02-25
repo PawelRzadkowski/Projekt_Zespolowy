37d78740be5df50a9bd31e9844085378710c96a2
PawelRzadkowski <147385702+PawelRzadkowski@users.noreply.github.com>
2026-02-25 23:34:52 +0100

    opis
    komentarze funkcji + opis pliku

--- todo-appka/backend/database.py
+++ todo-appka/backend/database.py
@@ -1,13 +1,18 @@
+#Plik odpowiedzialny za konfigurację połączenia z bazą danych SQLite oraz za tworzenie sesji i podstawowego obiektu Base dla modeli SQLAlchemy.
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
 
+# Tworzenie silnika połączenia z bazą danych
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# Konfiguracja sesji do pracy z bazą
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Obiekt bazowy dla wszystkich modeli
 Base = declarative_base()
